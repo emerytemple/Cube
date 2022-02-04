ecc89bb8e1718ac39e0dafa0a1c6b064c3931382
Emery Temple <emerytemple@gmail.com>
2022-02-04 18:57:22 -0500

    keyboard arrow functionality

--- main.c
+++ main.c
@@ -13,22 +13,30 @@
 
 SDL_Window *create_app();
 void destroy_app(SDL_Window *window);
+void handle_event(bool *quit, SDL_Event *event);
+void handle_keyboard_event(SDL_KeyboardEvent key);
 
 int main(int argc, char *argv[])
 {
 	SDL_Window *window = create_app();
 
+	bool quit = false;
 	SDL_Event event;
 
-	SDL_Surface *surface = SDL_GetWindowSurface(window);
-	const SDL_Rect *rect = NULL;
-	Uint32 color = SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF);
-	int fill_success = SDL_FillRect(surface, rect, color);
+	while(!quit)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			handle_event(&quit, &event);
+		}
 
-	SDL_UpdateWindowSurface(window);
+		SDL_Surface *surface = SDL_GetWindowSurface(window);
+		const SDL_Rect *rect = NULL;
+		Uint32 color = SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF);
+		int fill_success = SDL_FillRect(surface, rect, color);
 
-	Uint32 ms = 2000;
-	SDL_Delay(ms);
+		SDL_UpdateWindowSurface(window);
+	}
 
 	destroy_app(window);
 
@@ -37,8 +45,7 @@ int main(int argc, char *argv[])
 
 SDL_Window *create_app()
 {
-	Uint32 init_flags = SDL_INIT_VIDEO;
-	int success = SDL_Init(init_flags);
+	int success = SDL_Init(SDL_INIT_VIDEO);
 	if(success != 0) printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
 
 	const char *title = "Cube";
@@ -59,7 +66,47 @@ void destroy_app(SDL_Window *window)
 	SDL_Quit();
 }
 
+void handle_event(bool *quit, SDL_Event *event)
+{
+	switch(event->type)
+	{
+		case SDL_QUIT:
+			*quit = true;
+			break;
+		case SDL_KEYDOWN:
+		case SDL_KEYUP:
+			handle_keyboard_event(event->key);
+			break;
+		default:
+			break;
+	}
+}
 
+void handle_keyboard_event(SDL_KeyboardEvent key)
+{
+	printf("type = %s, state = %s, repeat = %s\n",
+		(key.type == SDL_KEYDOWN) ? "keydown" : "keyup",
+		(key.state == SDL_PRESSED) ? "pressed" : "released",
+		(key.repeat != 0) ? "repeat" : "single"
+	);
+	switch(key.keysym.sym)
+	{
+		case SDLK_UP:
+			puts("up");
+			break;
+		case SDLK_DOWN:
+			puts("down");
+			break;
+		case SDLK_LEFT:
+			puts("left");
+			break;
+		case SDLK_RIGHT:
+			puts("right");
+			break;
+		default:
+			break;
+	}
+}
 
 
 
